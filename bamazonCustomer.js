05067f0809ea97a19db4355e919ceef07045f2b5
georgejincy <jincydon101@gmail.com>
2017-02-28 11:19:12 -0500

    Display all items available for sale on bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,4 +17,15 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected");
+  initializeApp();
 });
+
+var initializeApp = function(){
+
+	//Display all items available for sale on bamazon
+	var query = "SELECT * FROM products";
+    connection.query(query, function(err, res) {
+     console.table(res);
+    });
+
+};
