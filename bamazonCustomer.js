8347c9e32a6e77dbbd34bf966be9c9d22e70e576
georgejincy <jincydon101@gmail.com>
2017-02-28 15:30:25 -0500

    Fixed bug with update of products table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,6 +18,8 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected");
   initializeApp();
+  //connection.end();
+  
 });
 
 var initializeApp = function(){
@@ -25,15 +27,18 @@ var initializeApp = function(){
 	//Display all items available for sale on bamazon
 	var query = "SELECT * FROM products";
     connection.query(query, function(err, res) {
+    	if(err) throw err;
     console.table(res);
-    var validItemID = [];
+    var pdtID = 0;
+    var quantity = 0;
+   /* var validItemID = [];
     for(i = 0, j = res.length; i<j; i++){
     	validItemID.push(res[i].item_id);
     }
 
 	for(i = 0, j = validItemID.length; i<j; i++){
     	console.log(validItemID[i]);
-    }
+    }*/
 
 
      //Prompt users for product ID and quantity
@@ -53,6 +58,7 @@ var initializeApp = function(){
   	}).then(function(answer) {
     
     	console.log(answer.productID);
+    	pdtID = answer.productID;
       
       //Prompt for quantity
       inquirer.prompt({
@@ -65,12 +71,39 @@ var initializeApp = function(){
 	    }
 	  	}).then(function(answer) {
 	  		console.log("Units: " + answer.quantity);
+	  		quantity = answer.quantity;
+	  		processOrder(pdtID, quantity);
 	  	});
 
     });
+
+
 
 });
     
 
 
 };
+
+var processOrder = function(id, quantity){
+	var query = "SELECT stock_quantity FROM products WHERE ?";
+	connection.query(query, {item_id: id}, function(err, res) {
+		//if(err) throw err;
+    	console.log(res[0].stock_quantity);
+
+    	if(res[0].stock_quantity < quantity){
+    		console.log("Insufficient quantity!");
+    	}else{
+    		updStock = res[0].stock_quantity - quantity;
+    		console.log(updStock);
+    		var query = "UPDATE products SET ? WHERE ?";
+    		connection.query(query, [{stock_quantity: updStock}, {item_id: id}], function(err, res) {
+    			//if(err) throw err;
+    			console.log("Order Placed! Thank you");
+    		});
+
+    	}
+	});
+	
+
+}
