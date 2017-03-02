de54aa1277d312df3d8f26aeb7c168ad98e432f9
georgejincy <jincydon101@gmail.com>
2017-03-02 13:03:32 -0500

    initial logic layout for supervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -20,3 +20,38 @@ connection.connect(function(err) {
   initializeApp();
   
 });
+
+//This function allows prompts user for various options or exit the application
+var initializeApp = function(){
+
+  inquirer.prompt([
+  {
+    type: 'list',
+    name: 'action',
+    message: 'What do you want to do?',
+    choices: [
+      'View Product Sales by Department',
+      'Create New Department',
+      'Exit application'
+    ]
+  }
+]).then(function (answer) {
+    //console.log(JSON.stringify(answer, null, '  '));
+  switch (answer.action) {
+      case "View Product Sales by Department":
+        console.log("View Product Sales by Department");
+        //viewProductSales();
+        break;
+      case "Create New Department":
+        console.log("Create New Department");
+        //addDepartment();
+        break;
+      
+      case "Exit application":
+        //end mysql connection
+        connection.end();
+        break;
+  }
+});
+
+};
